d9ff0bd03f45c2e77f1eeabdbf78b085b9366635
iki1uc <entertrainer@web.de>
2026-07-03 22:36:13 +0200

    Update online.js

--- online.js
+++ online.js
@@ -1,1 +1,11 @@
+export const ONLINE = () => {
+  return {
+    visitors: Math.floor(Math.random() * 5),
+    active: ["WpiR","dir","RUN8","FIT","9vec3tor","3hit90"],
+    working: ["RUN8","FIT"],
+    waiting: ["FAIL"],
+    ping: Math.floor(Math.random() * 100),
+    time: Date.now()
+  };
+};
 

39d1f66acd201255b155a6ebfef2b99f2460aeee
iki1uc <entertrainer@web.de>
2026-07-03 22:37:07 +0200

    Add MASTER function to export online status

--- master.js
+++ master.js
@@ -1,1 +1,13 @@
+export const MASTER = (online) => {
+  return {
+    master: "3hit90",
+    cpu: 90,
+    ram: 90,
+    gpu: 90,
+    status: "clean",
+    visitors: online.visitors,
+    working: online.working.length,
+    waiting: online.waiting.length
+  };
+};
 

dcc111e7fa418d2b5e372d75d873126d6aa8fb4e
iki1uc <entertrainer@web.de>
2026-07-03 22:40:11 +0200

    Update online.js

--- online.js
+++ online.js
@@ -1,6 +1,13 @@
 export const ONLINE = () => {
+  const clone = window.CLONE || { x:0, y:0, z:0 };
+
   return {
     visitors: Math.floor(Math.random() * 5),
+    clone: {
+      x: clone.x,
+      y: clone.y,
+      z: clone.z
+    },
     active: ["WpiR","dir","RUN8","FIT","9vec3tor","3hit90"],
     working: ["RUN8","FIT"],
     waiting: ["FAIL"],
@@ -8,4 +15,3 @@ export const ONLINE = () => {
     time: Date.now()
   };
 };
-

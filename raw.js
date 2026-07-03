dec3697e57aa93fba8e075d188cf04b28ba22f7d
iki1uc <entertrainer@web.de>
2026-07-04 00:46:57 +0200

    Update raw.js

--- raw.js
+++ raw.js
@@ -1,13 +1,20 @@
-export const RESPO_MAP = {
-  "WpiR":      "/WpiR/index.html",
-  "dir":       "/dir/index.html",
-  "RUN8":      "/RUN8/index.html",
-  "FIT":       "/FIT/index.html",
-  "FAIL":      "/FAIL/index.html",
-  "9vec3tor":  "/9vec3tor/index.html",
-  "CLONE":     "/CLONE/index.html",
-  "ONLINE":    "/online/index.html",
-  "TMP":       "/TMP/index.html",
-  "3hit90":    "/3hit90/index.html"
+export const RAW = {
+  RUN8:  { status: rnd(), tmp: tmp() },
+  TMP:   { status: rnd(), tmp: tmp() },
+  "3hit90": { status: rnd(), tmp: tmp() },
+  dir:   { status: rnd(), tmp: tmp() },
+  WpiR:  { status: rnd(), tmp: tmp() },
+  MXU:   { status: rnd(), tmp: tmp() },
+  CLONE: { status: rnd(), tmp: tmp() },
+  FAIL:  { status: rnd(), tmp: tmp() },
+  "9vec3tor": { status: rnd(), tmp: tmp() }
 };
 
+function rnd(){
+  const s = ["green","yellow","red","blue"];
+  return s[Math.floor(Math.random()*s.length)];
+}
+
+function tmp(){
+  return (Math.random()*0.999).toFixed(3);
+}

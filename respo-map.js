e1af502f16056d7dc619e5bd48af6441e6dab68a
iki1uc <entertrainer@web.de>
2026-07-03 23:15:16 +0200

    Update respo-map.js

--- respo-map.js
+++ respo-map.js
@@ -1,1 +1,13 @@
+export const RESPO_MAP = {
+  "WpiR": "/WpiR/index.html",
+  "dir": "/dir/index.html",
+  "RUN8": "/RUN8/index.html",
+  "FIT": "/FIT/index.html",
+  "FAIL": "/FAIL/index.html",
+  "9vec3tor": "/9vec3tor/index.html",
+  "CLONE": "/CLONE/index.html",
+  "ONLINE": "/online/index.html",
+  "TMP": "/TMP/index.html",
+  "3hit90": "/3hit90/index.html"
+};
 

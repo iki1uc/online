bffc442f98432aa7422206d253509ca237c0fecb
iki1uc <entertrainer@web.de>
2026-07-03 22:43:13 +0200

    Add clone parameter and restructure MASTER function

--- master.js
+++ master.js
@@ -1,10 +1,27 @@
-export const MASTER = (online) => {
+export const MASTER = (online, clone) => {
   return {
     master: "3hit90",
+
+    // 3hit90 bleibt sauber
     cpu: 90,
     ram: 90,
     gpu: 90,
-    clone_hit: (online.clone.x + online.clone.y + online.clone.z) / 3,
+
+    // echte Prüfung
+    clone_hit: {
+      x: clone.x,
+      y: clone.y,
+      z: clone.z,
+      avg: ((clone.x + clone.y + clone.z) / 3).toFixed(2)
+    },
+
+    online_hit: {
+      visitors: online.visitors,
+      working: online.working.length,
+      waiting: online.waiting.length,
+      ping: online.ping
+    },
+
     status: "clean"
   };
 };
